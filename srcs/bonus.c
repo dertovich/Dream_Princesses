c0efb08409b280c679df3f51c8352c761eb6b0d4
dertovich <86295099+dertovich@users.noreply.github.com>
2023-05-17 02:16:49 +0300

    Add files via upload

---
+++ srcs/bonus.c
@@ -0,0 +1,11 @@
+#include "../so_long.h"
+
+void	counter_bonus(t_vars *vars)
+{
+	const char		*str = ft_itoa(vars->counter);
+
+	if (!str)
+		__exit__(NULL, vars, FAILURE);
+	mlx_string_put(vars->mlx, vars->win, 100, 100, 0xFF0000, (char *)str);
+	free((char *)str);
+}
---
+++ srcs/textures.c
@@ -0,0 +1,32 @@
+#include "../so_long.h"
+
+void	set_tex(char *path, t_vars *vars, t_img *tex)
+{
+	const int	fd = open(path, O_RDONLY);
+
+	if (fd < 0)
+	{
+		ft_putstr("Error: Bad images path !\n");
+		exit(0);
+	}
+	tex->img = mlx_xpm_file_to_image(vars->mlx, path, &tex->width,
+			&tex->height);
+	if (!tex->img)
+		__exit__("path Error\n", vars, FAILURE);
+	tex->addr = mlx_get_data_addr(tex->img, &tex->bits_per_pixel,
+			&tex->line_len, &tex->endian);
+}
+
+void	tex_all(t_vars *vars)
+{
+	set_tex(TEX_PLAYER, vars, &vars->tex[0]);
+	set_tex(TEX_WALL, vars, &vars->tex[1]);
+	set_tex(TEX_COLLECT, vars, &vars->tex[2]);
+	set_tex(TEX_EXIT, vars, &vars->tex[3]);
+	set_tex(TEX_ZERO, vars, &vars->tex[4]);
+	set_tex(TEX_ENEMY, vars, &vars->tex[5]);
+	set_tex(TEX_PLAYER2, vars, &vars->tex[6]);
+}
+
+//	set_tex(TEX_PLAYER3, vars, &vars->tex[7]);
+//	set_tex(TEX_PLAYER4, vars, &vars->tex[8]);
